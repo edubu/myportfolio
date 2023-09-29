576d01dcbc442230eef4adfe1c3d0fa8866bf643
edubu <edubu@umich.edu>
2023-09-29 11:35:27 -0400

    adding own information

--- components/layout/DefaultLayout.jsx
+++ components/layout/DefaultLayout.jsx
@@ -1,16 +1,16 @@
-import AppHeader from '../shared/AppHeader';
-import AppFooter from '../shared/AppFooter';
-import PagesMetaHead from '../PagesMetaHead';
+import AppHeader from "../shared/AppHeader";
+import AppFooter from "../shared/AppFooter";
+import PagesMetaHead from "../PagesMetaHead";
 
 const DefaultLayout = ({ children }) => {
-	return (
-		<>
-			<PagesMetaHead />
-			<AppHeader />
-			<div>{children}</div>
-			<AppFooter />
-		</>
-	);
+  return (
+    <>
+      <PagesMetaHead title="Elliot Dubuque" />
+      <AppHeader />
+      <div>{children}</div>
+      <AppFooter />
+    </>
+  );
 };
 
 export default DefaultLayout;
